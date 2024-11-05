cfedd9225f43f4c6b312b9f9c24d62b1f21c351b
Alexandr <super.ab31144@gmail.com>
2024-11-05 06:42:31 +0400

    Add files via upload

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiningMinerals
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Data Source=MiningDB.db;";
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = AuthLogintext.Text;
+            string password = AuthPasstext.Password;
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM Accounts WHERE Login_Data = @username AND Password_Data = @password";
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                       MainWindow mainWindow = new MainWindow();
+                       mainWindow.Show();
+                       this.Close();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Невереный логин или пароль!");
+                    }
+                }
+            }
+        }
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ RegistrationWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiningMinerals
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+        string connectionString = "Data Source=MiningDB.db;";
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = RegLogintext.Text;
+            string password = RegPasstext.Password;
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "INSERT INTO Accounts (Login_Data, Password_Data) VALUES (@username, @password)";
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    int result = command.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Ваш аккаунт создан");
+                        LoginWindow loginWindow = new LoginWindow();
+                        loginWindow.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка регистрации");
+                    }
+                }
+            }
+        }
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
+        }
+    }
+}
